810339fa5875cf1be1840443e62f5b7cf3706972
nikhithapanasa550-boop <nikhithapanasa550@gmail.com>
2026-05-27 06:24:49 +0530

    Add: python-unpacking - submission-5

---
+++ Python Coding Interviews/python-unpacking/submission-5.py
@@ -0,0 +1,23 @@
+
+def sum_3_integers(triplet:list[int]):
+    x1,x2,x3=triplet
+    return x1+x2+x3 
+sum_3_integers([1,2,3])
+sum_3_integers([4,6,2])
+def compute_volume(box_dimensions: 
+    Tuple[int,int,int]):
+    width,height,depth=box_dimensions
+    return width*height*depth
+
+
+
+    pass
+  
+
+# do not modify below this line
+print(sum_3_integers([1, 2, 3]))
+print(sum_3_integers([4, 6, 2]))
+
+print(compute_volume((1, 2, 3)))
+print(compute_volume((3, 2, 1)))
+print(compute_volume((3, 9, 7)))
